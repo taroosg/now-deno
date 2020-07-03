8d4445ed61bce0e20e6b2ceeca79fdea0ddee348
taroosg <macchinetta.xyz@gmail.com>
2020-07-03 17:27:31 +0900

    fix json

--- api/test.ts
+++ api/test.ts
@@ -1,20 +1,24 @@
 import { ServerRequest } from "https://deno.land/std@0.58.0/http/server.ts";
 import { createApp } from "https://servestjs.org/@v1.1.0/mod.ts";
 
-export default async (req: ServerRequest) => {
-  // req.respond({ body: `Hell Wird!` });
-  const app = createApp();
-  app.get("/", async (req) => {
-    await req.respond({
-      status: 200,
-      body: "Hell Word",
-    });
+// req.respond({ body: `Hell Wird!` });
+const app = createApp();
+app.get("/", async (req) => {
+  await req.respond({
+    status: 200,
+    body: "Hell Word",
   });
-  app.get("/hoge", async (req) => {
-    await req.respond({
-      status: 200,
-      body: "hogehoge",
-    });
+});
+app.get("/hoge", async (req) => {
+  await req.respond({
+    status: 200,
+    body: "hogehoge",
   });
-  app.listen({ port: 8000 });
-};
+});
+app.listen({ port: 8000 });
+
+export default app;
+
+// export default async (req: ServerRequest) => {
+
+// };
